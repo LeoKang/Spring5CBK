9b526c0ef9bc3bfbb7ff60a72a2a652cff64d6d2
Seungiin Kang-Shingil <xeungjin@nate.com>
2025-03-26 17:10:23 +0900

    46 page 첫 번째 예제

---
+++ src/test/java/book/game/spring5fs/Spring5fsApplicationTests.java
@@ -0,0 +1,13 @@
+package book.game.spring5fs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5fsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
